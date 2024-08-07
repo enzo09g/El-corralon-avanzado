a945a50afd6fac9b80bd906bcdcd1ee39abc3ef3
Enzo Gómez <enzoeegv@gmail.com>
2024-08-07 07:00:13 -0300

    añadido efecto con el tranlate

--- js/navbarToggle.js
+++ js/navbarToggle.js
@@ -12,10 +12,33 @@ document.addEventListener('DOMContentLoaded', () => {
 
     })
 
+    backdropMenu()
 })
 
 function ToggleFunction() {
     console.log("Hola")
     const menu = document.getElementById('toggleMenu');
     menu.classList.toggle('active')
+}
+
+function backdropMenu() {
+    const li = document.querySelectorAll('.visibleUl li');
+    const menuBackdrop = document.querySelector('#menuBackdrop');
+    console.log(li)
+
+    li.forEach(item => {
+        item.addEventListener('mouseenter', () => {
+            const { left, top, width, height } = item.getBoundingClientRect();
+            menuBackdrop.style.setProperty("--left", `${left}px`)
+            menuBackdrop.style.setProperty("--top", `${top}px`)
+            menuBackdrop.style.setProperty("--width", `${width}px`)
+            menuBackdrop.style.setProperty("--height", `${height}px`)
+            menuBackdrop.style.setProperty("opacity", 1);
+        })
+
+        item.addEventListener('mouseleave', () => {
+            menuBackdrop.style.setProperty("opacity", 0);
+        })
+    });
+
 }
