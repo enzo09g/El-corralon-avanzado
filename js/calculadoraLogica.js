e876bdfab6460379ca9ed259e4b2b7b21a1bc175
Enzo Gómez <enzoeegv@gmail.com>
2024-10-16 16:00:32 -0300

    Colocar dimensiones del contenedor terminado

--- js/calculadoraLogica.js
+++ js/calculadoraLogica.js
@@ -1,21 +1,48 @@
 document.addEventListener('DOMContentLoaded', () => {
     agregarEventos()
+    obtenerAltoContenedor()
 })
 
 const tomarDimensiones = () => {
     const inputs = document.querySelectorAll('input');
-    const lado1 = inputs[0].value
-    const lado2 = inputs[1].value
+    const alto = inputs[0].value
+    const ancho = inputs[1].value
 
-    console.log(lado1 * lado2)
-
-    return lado1 * lado2;
-
+    return alto / ancho;
 }
 
 const agregarEventos = () => {
     const inputs = document.querySelectorAll('input');
     inputs.forEach(input => {
-        input.addEventListener('input', tomarDimensiones)
+        input.addEventListener('input', colocarLargo)
     })
+}
+
+const obtenerAltoContenedor = () => {
+    const cont = document.querySelector('#contenedorCieloRraso');
+    const { height } = cont.getBoundingClientRect()
+    return height;
+}
+
+function colocarLargo() {
+    const largo = obtenerLargo();
+    const claseActual = obtenerClase()
+    const nuevaClase = `w-[${largo}px]`
+
+    const contenedor = document.querySelector('#contenedorCieloRraso');
+    contenedor.classList.remove(claseActual);
+    contenedor.classList.add(nuevaClase)
+}
+
+const obtenerLargo = () => {
+    const proporcion = tomarDimensiones();
+    const alto = obtenerAltoContenedor();
+    return alto / proporcion;
+}
+
+const obtenerClase = () => {
+    const contenedor = document.querySelector('#contenedorCieloRraso');
+    const arrayClases = Array.from(contenedor.classList);
+    return claseEnconrada = arrayClases.find(clase => clase.includes('w-['))
+
 }
